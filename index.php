ca502a44509e3f6f08a65dc439733d3cd4ae230d
omer <omerasafozdemir20@gmail.com>
2024-03-19 17:43:55 +0300

    firs commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Todo</title>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="create-todo">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="navbars">
+                    <img src="icon/checklist.png" width="30">
+                    <h4>Tasks Lists</h4>
+                </div>
+
+                <div class="content">
+                    <hr>
+
+                    <ul>
+
+                        <li>
+                            <input type="checkbox" name="">
+                            <p>Gününü Tamamla</p>
+
+                            <small><a href=""><img width="30" src="icon/delete.png"></a></small>
+                        </li>
+                        <hr>
+
+
+                    </ul>
+
+                </div>
+
+
+
+
+            </div>
+
+
+        </div>
+    </div>
+
+</body>
+
+</html>
